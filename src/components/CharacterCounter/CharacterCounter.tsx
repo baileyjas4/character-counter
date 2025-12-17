a11bd24feda4fdd64b29c5a330eba58fb9951c41
baileyjas4 <baileyjasmine4@icloud.com>
2025-12-17 14:37:36 -0500

    import files into character counter

---
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -0,0 +1,6 @@
+import React, { useState, useCallback } from "react";
+import { TextInput } from "../TextInput/TextInput";
+import { StatsDisplay } from "../StatsDisplay/StatsDisplay";
+import { CharacterCounterProps, TextStats } from "../../types";
+
+export const CharacterCounter: React
